71c49d34ac1e1f1ea8a756a4463d58f0e7374da2
Pau <pau.guri@mortensen.cat>
2025-12-12 13:27:51 +0100

    hardcode env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = "https://isghotnbpvxswndongod.supabase.co";
+const supabaseKey = "sb_publishable_YxvLPlhml-aLDernspfsLg_y2KRlUWy";
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase environment variables');
+  throw new Error("Missing Supabase environment variables");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
